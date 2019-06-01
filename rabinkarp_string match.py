14414993ab5a7cc9dec2f5e602e7f3febf7228ca
Prajwal102 <prajwal.pathak102@gmail.com>
2019-06-02 00:26:53 +0530

    comments

--- rabinkarp_string match.py
+++ rabinkarp_string match.py
@@ -10,7 +10,7 @@ text_len = len(text)
 
 text_hash,pattern_hash = 0,0
 
-
+# initially find hash for text and pattern
 for i in range(pattern_len):
     text_hash += ord(text[i]) * (26 ** (pattern_len-i-1))
     pattern_hash += ord(pattern[i]) * (26 ** (pattern_len-i-1))
@@ -18,6 +18,9 @@ for i in range(pattern_len):
 
 
 
+#compare if the 2 hashes match
+# if the hashes match then check character by character
+# finally shift for next text
 for i in range(text_len-pattern_len+1):
     # print(i,text[i:i+pattern_len],text_hash)
     if(text_hash == pattern_hash):
@@ -26,7 +29,9 @@ for i in range(text_len-pattern_len+1):
             break
            
 
-   
+   # find next hash from the previous hash
+   # H("bcd") = H("abc") - H("a") + H("d")
+   # https://brilliant.org/wiki/rabin-karp-algorithm/
     temp =  ord(text[i]) * (26 ** (pattern_len -1))
     text_hash = prev_text_hash - temp
     text_hash = text_hash * 26
