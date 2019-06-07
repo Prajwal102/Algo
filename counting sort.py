2cb4d5b63a3a63415ab58edef1039d0192d17afc
Prajwal102 <prajwal.pathak102@gmail.com>
2019-06-08 01:37:51 +0530

    modified

--- counting sort.py
+++ counting sort.py
@@ -13,28 +13,31 @@ def counting_sort(arr):
     for i in arr: 
         count[i] = count[i] + 1
     
-    
+    print(arr)
+    print(count)
     # cumulatively add the counts
     for i in range(1,len(count)):
         count[i] = count[i] + count[i-1]
         
+
+    print("cumulative: ",count)
     # shift count to the right 
-    for i in range(len(count)-1,-1,-1):
-        count[i] = count[i-1]
-    count[0] = 0
+    # for i in range(len(count)-1,-1,-1):
+    #     count[i] = count[i-1]
+    # count[0] = 0
 
     
     
     # consult arr and count to fill in array c
     for i in range(len(arr)):
-        c[count[arr[i]]] = arr[i]
-        count[arr[i]] = count[arr[i]] + 1
+        c[count[arr[i]] - 1] = arr[i]
+        count[arr[i]] = count[arr[i]] - 1
     
     
 
     print(c)
 
 
-a = [9, 4, 1, 7, 9, 1, 2, 0]
+a = [9, 4, 1, 7, 9, 1, 2, 3]
 counting_sort(a)
 
